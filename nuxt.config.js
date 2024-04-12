f890e0c066cda19f978b0271f722ec905f0bc867
pranalidevkar1311 <pranali.devkar@machintel.net>
2024-04-12 18:37:38 +0530

    fevicon changes

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,7 +13,7 @@ export default {
     meta: [
       { name: "viewport", content: "width=device-width, initial-scale=1" },
     ],
-    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+    link: [{ rel: "icon", type: "image/x-icon", href: "/fevicon-bgi.png" }],
     script: [
       // <script src="https://myawesome-lib.js"></script>
 
